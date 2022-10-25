b9009bd0230341ed0e9a0fd0c8d57a5157fbd889
Lucas Vieillevigne <lvieillevigne@normandiewebschool.fr>
2022-10-25 20:37:42 +0200

    Student.php

---
+++ class/Student.php
@@ -0,0 +1,25 @@
+<?php
+require_once "Database.php";
+
+class Student extends Database{
+    public function addRegister($data){
+        $prenom = $data['first-name'];
+        $nom = $data['last-name'];
+        $age = $data['age'];
+        $email = $data['email'];
+        $tel = $data['tel'];
+        $level = $data['level'];
+        $filiere = $data['filiere'];
+
+        if(isset($prenom) && isset($nom) && isset($age) && isset($email) && isset($tel) && isset($level) && isset($filiere)){
+            $query = "INSERT INTO etudiant (prenom, nom, age, email, tel, level, filiere) VALUES (?,?,?,?,?,?,?)";             
+            $request = $this->getPDO()->prepare($query);            
+            $request->execute([$prenom, $nom, $email, $age, $email, $tel, $level, $filiere]);
+        }
+        else{
+            $msg = "Tous les champs sont obligatoires.";             
+            return $msg;
+        }
+    }
+    
+}
